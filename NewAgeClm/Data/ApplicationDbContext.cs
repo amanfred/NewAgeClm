c11dbfb233a67ce3cbe4e92b37333322057d7303
amanfred <alesmanfreda82@gmail.com>
2019-03-24 18:08:37 +0100

    reset

--- NewAgeClm/Data/ApplicationDbContext.cs
+++ NewAgeClm/Data/ApplicationDbContext.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using NewAgeClm.Areas.RegularUser.Models;
 using NewAgeClm.Models;
 
 namespace NewAgeClm.Data
@@ -14,9 +13,9 @@ namespace NewAgeClm.Data
 			: base(options)
 		{
 		}
-
+		/*
 		public DbSet<Projects> Projects { get; set; }
 		public DbSet<ProjectTypes> ProjectTypes { get; set; }
-		public DbSet<Priorities> Priorities { get; set; }
+		public DbSet<Priorities> Priorities { get; set; }*/
 	}
 }
